1873e32b1d0724310022107cf8890f5318a420c3
haohtpc05526 <trunghaopc23579@gmail.com>
2024-02-02 14:22:15 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const useName = 'James';
+
+const person = {first: useName};
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+    console.log(`Hello Linting, ${fName}`);
+};
+
+sayHelloLinting('Hào');
